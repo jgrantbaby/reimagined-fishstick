342b658df471e9d39c3b80c9a3f840baf4ca76fa
jgrantbaby <39221653+jgrantbaby@users.noreply.github.com>
2025-06-10 14:34:13 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -4,35 +4,10 @@ import simpleGit from "simple-git";
 import random from "random";
 
 const path = "./data.json";
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
 
-const markCommit = (x, y) => {
-  const date = moment()
-    .subtract(1, "y")
-    .add(1, "d")
-    .add(x, "w")
-    .add(y, "d")
-    .format();
-  const data = { date: date };
-  jsonfile.writeFile(path, data, () => {
-    simpleGit()
-      .pull("origin", "main")
-      .then(() => {
-        simpleGit()
-          .add([path])
-          .commit(date, { "--date": date })
-          .push("origin", "main", { "--force": true }, (err) => {
-            if (err) {
-              console.error("Push error:", err);
-            } else {
-              console.log("Push successful!");
-            }
-          });
-      })
-      .catch((err) => {
-        console.error("Pull error:", err);
-      });
-  });
-};
 
 let counter = 0;
 
@@ -44,7 +19,7 @@ const makeCommits = (n) => {
   const x = random.int(0, 54);
   const y = random.int(0, 6);
   const date = moment()
-    .subtract(1, "y")
+    .subtract(getRndInteger(1,3), "y")
     .add(1, "d")
     .add(x, "w")
     .add(y, "d")
@@ -74,4 +49,4 @@ const makeCommits = (n) => {
   });
 };
 
-makeCommits(1023);
+makeCommits(1034);
