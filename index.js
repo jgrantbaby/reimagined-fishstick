626db271273a6130a4df5f84b42d30c96f29dd6d
jgrantbaby <39221653+jgrantbaby@users.noreply.github.com>
2025-06-10 14:13:52 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = "./data.json";
 
 const markCommit = (x, y) => {
   const date = moment()
-    .subtract(5, "y")
+    .subtract(1, "y")
     .add(1, "d")
     .add(x, "w")
     .add(y, "d")
@@ -44,7 +44,7 @@ const makeCommits = (n) => {
   const x = random.int(0, 54);
   const y = random.int(0, 6);
   const date = moment()
-    .subtract(4, "y")
+    .subtract(1, "y")
     .add(1, "d")
     .add(x, "w")
     .add(y, "d")
@@ -74,4 +74,4 @@ const makeCommits = (n) => {
   });
 };
 
-makeCommits(10000003);
+makeCommits(1023);
